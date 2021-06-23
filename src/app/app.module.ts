503962ea9f4b1a6aa2c4c0b36ace30d541c271e7
Otavio Monteagudo <oivatom@gmail.com>
2021-06-23 00:39:08 -0300

    declared external modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/* added from angular internals */
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+/* added from external angular packages */
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+/* added from external node packages */
+import { GaugeModule } from 'angular-gauge';
+
 @NgModule({
   declarations: [
     AppComponent
