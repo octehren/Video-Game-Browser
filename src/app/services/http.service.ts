fbdf7d0ecc9c88729e73ea10d68afdea9d80eecc
Otavio Monteagudo <oivatom@gmail.com>
2021-07-02 04:27:11 -0300

    importing interfaces in service

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment as env } from 'src/environments/environment';
+import { APIResponse, Game } from 'src/models';
 
 @Injectable({
   providedIn: 'root'
