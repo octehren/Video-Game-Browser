d47816c228a96462f54f9692e5ea3983fd25e858
Otavio Monteagudo <oivatom@gmail.com>
2021-07-02 07:25:51 -0300

    fix

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment as env } from 'src/environments/environment';
 import { APIResponse, Game } from 'src/models';
 import { Observable } from "rxjs";
+
 @Injectable({
   providedIn: 'root'
 })
