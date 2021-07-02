f771dba81dcdd003f8ea89a6c30d6924648a4f6b
Otavio Monteagudo <oivatom@gmail.com>
2021-07-02 04:16:51 -0300

    added base url to env

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment as env } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
