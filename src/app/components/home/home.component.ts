3261c9fc4850e367046283a39a2c3c9f74b7b2b2
Otavio Monteagudo <oivatom@gmail.com>
2021-07-03 11:23:33 -0300

    fix: refactor subscribers to prevent memory leak

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { HttpService } from 'src/app/services/http.service';
 import { APIResponse, Game } from 'src/models';
 
@@ -11,11 +12,16 @@ import { APIResponse, Game } from 'src/models';
 export class HomeComponent implements OnInit {
   public sortBy!: string;
   public games?: Array<Game>;
+  // refactor: register subscriptions as attributes to avoid memory leaks
+  private routeSub?:Subscription;
+  private gameSub?:Subscription;
+  
 
   // connect http service & router
   constructor(
     private httpService: HttpService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) { 
     this.initialize(); // needs to be initialized
   }
@@ -25,7 +31,7 @@ export class HomeComponent implements OnInit {
   }
   // on first render, fetch game data sorted by metacritic score
   ngOnInit(): void { // first phase in component lifecycle
-    this.activatedRoute.params.subscribe((params: Params) => {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
       if (params['game-search']) { // if there are search params, perform search
         this.fetchGamesData('metacrit', params['game-search']); // sort results by score by default
       } else {
@@ -35,7 +41,7 @@ export class HomeComponent implements OnInit {
   }
 
   fetchGamesData(sortBy: string, search?: string):void {
-    this.httpService
+    this.gameSub = this.httpService
       .getGameList(sortBy, search)
       .subscribe((gameList: APIResponse<Game>) => {
         this.games = gameList.results;
@@ -44,6 +50,7 @@ export class HomeComponent implements OnInit {
   }
 
   openGameDetails(gameId: number):void {
+    this.router.navigate(['details', gameId]);
     console.log("Opening details page for Game #", gameId, "\n");
   }
 
