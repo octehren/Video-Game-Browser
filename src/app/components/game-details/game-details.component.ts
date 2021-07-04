96d93b495506332ff0089a8ca5655a68ea456728
Otavio Monteagudo <oivatom@gmail.com>
2021-07-04 15:35:33 -0300

    added methods to get further details for a game

--- src/app/components/game-details/game-details.component.ts
+++ src/app/components/game-details/game-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { HttpService } from 'src/app/services/http.service';
+import { Game } from 'src/models';
 
 @Component({
   selector: 'app-game-details',
@@ -6,12 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-details.component.scss']
 })
 export class GameDetailsComponent implements OnInit {
-  public gameRating:number = 0;
+  public gameRating: number = 0;
+  public gameId!: string;
+  public game!: Game;
+  private routeSub?: Subscription;
+  private gameSub?: Subscription;
 
-  constructor() { }
+  constructor(
+    private  ActivatedRoute: ActivatedRoute, // provides with API of the route once activated
+    private httpService: HttpService // custom service to communicate with api
+  ) { }
 
   ngOnInit(): void {
+    // activatedRoute gets from active route in url bar
+    this.routeSub = this.ActivatedRoute.params.subscribe((params: Params) => {
+      this.gameId = params['gameId'];
+      this.setGameDetails(this.gameId);
+    });
+  }
 
+  setGameDetails(id: string): void {
+    this.gameSub = this.httpService
+      .getGameDetails(id)
+      .subscribe((gameResp: Game) => {
+        this.game = gameResp;
+        setTimeout(() => {
+          this.gameRating = this.game.metacritic;
+        })
+      });
   }
 
   getColor(value: number): string {
@@ -21,7 +47,7 @@ export class GameDetailsComponent implements OnInit {
     } else if (value > 50) {
       hexColor = "#fffa50";
     } else if (value > 30) {
-      hexColor = "f7aa38";
+      hexColor = "#f7aa38";
     } else {
       hexColor = "#ef4655";
     }
