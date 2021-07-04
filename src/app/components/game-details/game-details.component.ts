a87bfca6d05a624be9ae5bcd5ab7b3c79fbf5c32
Otavio Monteagudo <oivatom@gmail.com>
2021-07-04 11:50:46 -0300

    feat: first draft of game-details component

--- src/app/components/game-details/game-details.component.ts
+++ src/app/components/game-details/game-details.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-details.component.scss']
 })
 export class GameDetailsComponent implements OnInit {
+  public gameRating:number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  getColor(value: number): string {
+    let hexColor:string;
+    if (value > 75) {
+      hexColor = "#5ee432";
+    } else if (value > 50) {
+      hexColor = "#fffa50";
+    } else if (value > 30) {
+      hexColor = "f7aa38";
+    } else {
+      hexColor = "#ef4655";
+    }
+    return hexColor;
   }
 
 }
