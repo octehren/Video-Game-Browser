0b48842950850cb008ec45e99ea5dd565a884b87
Otavio Monteagudo <oivatom@gmail.com>
2021-07-05 04:56:50 -0300

    passing game as input to child components

--- src/app/components/game-details-tabs/game-details-tabs.component.ts
+++ src/app/components/game-details-tabs/game-details-tabs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from 'src/models';
 
 @Component({
   selector: 'app-game-details-tabs',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-details-tabs.component.scss']
 })
 export class GameDetailsTabsComponent implements OnInit {
+  @Input() game!: Game;
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
